96992699ac395b1f7bead0e24728f00f869f38c1
realwrtoff <fmj_912@163.com>
2020-10-28 16:34:29 +0800

    local build env ok

---
+++ tyc/scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+source tmp/environment.sh
+
+function Trac() {
+    echo "[TRAC] [$(date +"%Y-%m-%d %H:%M:%S")] $1"
+}
+
+function Info() {
+    echo "\033[1;32m[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] $1\033[0m"
+}
+
+function Warn() {
+    echo "\033[1;31m[WARN] [$(date +"%Y-%m-%d %H:%M:%S")] $1\033[0m"
+    return 1
+}
+
+function Build() {
+    cd .. && make image && cd -
+    docker login --username="${RegistryUsername}" --password="${RegistryPassword}" "${RegistryServer}"
+    docker tag "${Image}:${Version}" "${RegistryServer}/${Image}:${Version}"
+    docker push "${RegistryServer}/${Image}:${Version}"
+}
+
+function Help() {
+    echo "sh deploy.sh <action>"
+    echo "example"
+    echo "  sh deploy.sh build"
+}
+
+function main() {
+    if [ -z "$1" ]; then
+        Help
+        return 0
+    fi
+
+    case "$1" in
+        "build") Build;;
+    esac
+}
+
+main "$@"
---
+++ tyc/scripts/environment.sh.tpl
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+Name="rest_grpc"
+RegistryServer="{{.registry.server}}"
+RegistryUsername="{{.registry.username}}"
+RegistryPassword="{{.registry.password}}"
+Image="realwrtoff/rest_grpc"
+Version="1.0.1"
